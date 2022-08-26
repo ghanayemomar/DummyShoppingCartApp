ec4e06a6bd00da113c9e9e650dacae392cf10ad1
gem00n <o.ghanaiem@student.aaup.edu>
2022-08-27 00:17:27 +0300

    144

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,16 +1,40 @@
 import React from "react";
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const defualCartState = {
+    item: [],
+    totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+    if(action.type === 'ADD'){
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    }
+    return defualCartState;
+};
+
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+
+  const [cartState,dispatchCartAction] = useReducer(cartReducer , defualCartState);
+
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({type: 'ADD' , item: item});
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({type:'REMOVE' , id: id});
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.item,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
+
   return (
     <CartContext.Provider value={cartContext}>
       {props.children}
