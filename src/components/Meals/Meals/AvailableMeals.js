02830487184e44483695d3af639d0e038c6a89b2
gem00n <o.ghanaiem@student.aaup.edu>
2022-09-10 04:14:24 +0300

    218_Error Handling

--- src/components/Meals/Meals/AvailableMeals.js
+++ src/components/Meals/Meals/AvailableMeals.js
@@ -7,11 +7,15 @@ import Card from "../../Ul/Card";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://react-meals-a46c3-default-rtdb.firebaseio.com/meals.json"
       );
+      if (!response.ok) {
+        throw new Error("Something Went Wrong!");
+      }
       const responseData = await response.json();
       const loadedMeals = [];
       for (const key in responseData) {
@@ -23,17 +27,30 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
-      setIsLoading(false)
+      setIsLoading(false);
     };
-    fetchMeals();
-    
+
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setError(error.message);
+    });
   }, []);
 
-  if(isLoading){
-    return <section className={classes.MealsLoading}>
-      <p>Loading...</p>
-    </section>
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
   }
+    if (error) {
+      return (
+        <section className={classes.MealsError}>
+          <p>{error}</p>
+        </section>
+      );
+    }
+  
   const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
@@ -55,3 +72,6 @@ export default AvailableMeals;
 //hint: Use Effect Should Return a function not reaturn a promise so youcant excute the fetch function directrly inside it you have to put it inside an function inside the useEffect
 //async await to make sure that the function if got the Data from the backend
 //loading handling is very nice we made an a return section with if statemnt to check if it loading and show the loading paragraph
+//using a sperate function of raping the logic is good for handling the error tho!
+// our way is an a traditional way to handling error inside a promise!!
+//like if we throw an error inside a promise that error will cause the promise to reject
